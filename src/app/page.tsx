423f9b2acb48121955d2ef3104fba68f22c94a46
Ashish Ekka <ashishekka329@gmail.com>
2025-06-22 15:45:40 +0000

    I can't see the yaml, inputs and inspect window. I guess because we have

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,7 +202,7 @@ export default function Home() {
       <Header />
       <main className="flex flex-1 overflow-hidden">
         <NodePalette onAddNode={addNode} />
-        <div className="flex-1 h-full" onMouseMove={handleNodeDrag} onMouseUp={handleNodeDragEnd} onClick={handleCanvasClick}>
+        <div className="flex-1 h-full min-w-0" onMouseMove={handleNodeDrag} onMouseUp={handleNodeDragEnd} onClick={handleCanvasClick}>
           <FlowEditor
             nodes={nodes}
             startNodeId={startNodeId}
